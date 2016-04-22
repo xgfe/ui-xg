4eb913edc763859284015167b98152c7b77bc2f2
YangJiyuan <yjy972080142@gmail.com>
2016-04-22 18:08:39 +0800

    test(datepicker):add test cases for datepicker

--- src/calendar/calendar.js
+++ src/calendar/calendar.js
@@ -81,6 +81,7 @@ angular.module('ui.fugu.calendar', ['ui.fugu.timepanel'])
                 $log.warn('Calendar directive: "ng-model" value must be a Date object, a number of milliseconds since 01.01.1970 or a string representing an RFC2822 or ISO 8601 date.');
                 date = new Date(); // fix #1 如果没有传入日期,或者清空的话,设置当前time
             }
+            date = new Date(date);
             $scope.selectDate = angular.copy(date);
 
             $scope.currentYear = $scope.selectDate.getFullYear();
