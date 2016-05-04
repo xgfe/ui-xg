cb2723fc83c3c2f8b92e1656fc3db24fe6dbeba3
YangJiyuan <yjy972080142@gmail.com>
2016-05-04 11:09:57 +0800

    chore(coverage):add coverage

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,8 @@ module.exports = function (config) {
         plugins: [
             'karma-junit-reporter',
             'karma-chrome-launcher',
-            'karma-jasmine'
+            'karma-jasmine',
+            'karma-coverage'
         ],
 
 
@@ -37,13 +38,17 @@ module.exports = function (config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: { 'src/*/*.js': ['coverage'] },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress','coverage'],
+        coverageReporter: {
+            type: 'lcov',
+            dir : 'coverage/'
+        },
 
 
         // web server port
