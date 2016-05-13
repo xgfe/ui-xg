fb6cae3d7212889409d2d808acd5be14d2f7a8c0
YangJiyuan <yjy972080142@gmail.com>
2016-05-13 19:04:29 +0800

    chore(site):add cdn path

--- misc/assets/js/main.js
+++ misc/assets/js/main.js
@@ -1,16 +1,24 @@
 require.config({
     urlArgs: '2016011903',
     paths: {
-        'jquery':'lib/jquery.min',
-        'angular': 'lib/angular.min',
-        'lib': 'lib/lib-comb'
+        'jquery':'http://cdn.bootcss.com/jquery/1.11.3/jquery.min',
+        'angular':'http://cdn.bootcss.com/angular.js/1.2.25/angular.min',
+        'ngAnimate':'http://cdn.bootcss.com/angular.js/1.2.25/angular-animate.min',
+        'uiRouter':'http://cdn.bootcss.com/angular-ui-router/0.2.15/angular-ui-router.min',
+        'uiFugu': 'lib/ui-fugu.min'
     },
     shim:{
         'angular': {
             'deps': ['jquery'],
             'exports': 'angular'
         },
-        'lib':{
+        'ngAnimate':{
+            'deps': ['angular']
+        },
+        'uiRouter':{
+            'deps': ['angular']
+        },
+        'uiFugu':{
             'deps': ['angular']
         }
     }
