444778410a0f6d9fae717e2734887cbf7f5edeb5
YangJiyuan <yjy972080142@gmail.com>
2016-05-16 15:30:25 +0800

    chore(main):change http into https

--- misc/assets/js/main.js
+++ misc/assets/js/main.js
@@ -1,10 +1,10 @@
 require.config({
     urlArgs: '2016011903',
     paths: {
-        'jquery':'http://cdn.bootcss.com/jquery/1.11.3/jquery.min',
-        'angular':'http://cdn.bootcss.com/angular.js/1.2.25/angular.min',
-        'ngAnimate':'http://cdn.bootcss.com/angular.js/1.2.25/angular-animate.min',
-        'uiRouter':'http://cdn.bootcss.com/angular-ui-router/0.2.15/angular-ui-router.min',
+        'jquery':'//cdn.bootcss.com/jquery/1.11.3/jquery.min',
+        'angular':'//cdn.bootcss.com/angular.js/1.2.25/angular.min',
+        'ngAnimate':'//cdn.bootcss.com/angular.js/1.2.25/angular-animate.min',
+        'uiRouter':'//cdn.bootcss.com/angular-ui-router/0.2.15/angular-ui-router.min',
         'uiFugu': 'lib/ui-fugu.min'
     },
     shim:{
