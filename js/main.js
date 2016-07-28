b54dbada99eee3c713e1968ce5d83f1888ac0b0e
YangJiyuan <yangjiyuan@meituan.com>
2016-07-28 14:08:29 +0800

    Update Site:2016-07-28 14:08

--- js/main.js
+++ js/main.js
@@ -1,31 +1,35 @@
 require.config({
     urlArgs: '2016011903',
     paths: {
-        'jquery':'//cdn.bootcss.com/jquery/1.11.3/jquery.min',
-        'angular':'//cdn.bootcss.com/angular.js/1.2.25/angular.min',
-        'ngAnimate':'//cdn.bootcss.com/angular.js/1.2.25/angular-animate.min',
-        'uiRouter':'//cdn.bootcss.com/angular-ui-router/0.2.15/angular-ui-router.min',
+        'jquery': '//cdn.bootcss.com/jquery/1.11.3/jquery.min',
+        'angular': '//cdn.bootcss.com/angular.js/1.2.25/angular.min',
+        'ngAnimate': '//cdn.bootcss.com/angular.js/1.2.25/angular-animate.min',
+        'uiRouter': '//cdn.bootcss.com/angular-ui-router/0.2.15/angular-ui-router.min',
+        'ngSanitize': '//cdn.bootcss.com/angular-sanitize/1.2.29/angular-sanitize.min',
         'uiXg': 'lib/ui-xg.min'
     },
-    shim:{
+    shim: {
         'angular': {
             'deps': ['jquery'],
             'exports': 'angular'
         },
-        'ngAnimate':{
+        'ngAnimate': {
             'deps': ['angular']
         },
-        'uiRouter':{
+        'ngSanitize': {
             'deps': ['angular']
         },
-        'uiXg':{
+        'uiRouter': {
+            'deps': ['angular']
+        },
+        'uiXg': {
             'deps': ['angular']
         }
     }
 });
-require(['app','angular','routers'], function(app, angular) {
-    angular.element(document).ready(function() {
-        angular.bootstrap(document, [app.name, function() {
+require(['app', 'angular', 'routers'], function (app, angular) {
+    angular.element(document).ready(function () {
+        angular.bootstrap(document, [app.name, function () {
             angular.element(document).find('html').addClass('ng-app');
         }]);
     });
