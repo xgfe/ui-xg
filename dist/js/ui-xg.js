6a4b1b412cc6011fe8037fad45df9cd4520e0ca9
xgfe <mtxgfe@gmail.com>
2016-08-22 15:27:18 +0800

    feat(typeahead): add typeahead. #11

--- dist/js/ui-xg.js
+++ dist/js/ui-xg.js
@@ -1,10 +1,10 @@
 /*
  * ui-xg
- * Version: 1.3.0 - 2016-08-17
+ * Version: 1.3.0 - 2016-08-22
  * License: MIT
  */
-angular.module("ui.xg", ["ui.xg.tpls","ui.xg.alert","ui.xg.button","ui.xg.buttonGroup","ui.xg.timepanel","ui.xg.calendar","ui.xg.transition","ui.xg.collapse","ui.xg.position","ui.xg.stackedMap","ui.xg.tooltip","ui.xg.popover","ui.xg.datepicker","ui.xg.dropdown","ui.xg.loader","ui.xg.modal","ui.xg.notify","ui.xg.pager","ui.xg.searchBox","ui.xg.select","ui.xg.sortable","ui.xg.switch","ui.xg.tableLoader","ui.xg.timepicker"]);
-angular.module("ui.xg.tpls", ["alert/templates/alert.html","button/templates/button.html","buttonGroup/templates/buttonGroup.html","timepanel/templates/timepanel.html","calendar/templates/calendar.html","tooltip/templates/tooltip-html-popup.html","tooltip/templates/tooltip-popup.html","tooltip/templates/tooltip-template-popup.html","popover/templates/popover-html-popup.html","popover/templates/popover-popup.html","popover/templates/popover-template-popup.html","datepicker/templates/datepicker-calendar.html","datepicker/templates/datepicker.html","modal/templates/backdrop.html","modal/templates/window.html","notify/templates/notify.html","pager/templates/pager.html","searchBox/templates/searchBox.html","select/templates/choices.html","select/templates/match-multiple.html","select/templates/match.html","select/templates/select-multiple.html","select/templates/select.html","switch/templates/switch.html","timepicker/templates/timepicker-timepanel.html","timepicker/templates/timepicker.html"]);
+angular.module("ui.xg", ["ui.xg.tpls","ui.xg.alert","ui.xg.button","ui.xg.buttonGroup","ui.xg.timepanel","ui.xg.calendar","ui.xg.transition","ui.xg.collapse","ui.xg.position","ui.xg.stackedMap","ui.xg.tooltip","ui.xg.popover","ui.xg.datepicker","ui.xg.dropdown","ui.xg.loader","ui.xg.modal","ui.xg.notify","ui.xg.pager","ui.xg.searchBox","ui.xg.select","ui.xg.sortable","ui.xg.switch","ui.xg.tableLoader","ui.xg.timepicker","ui.xg.typeahead"]);
+angular.module("ui.xg.tpls", ["alert/templates/alert.html","button/templates/button.html","buttonGroup/templates/buttonGroup.html","timepanel/templates/timepanel.html","calendar/templates/calendar.html","tooltip/templates/tooltip-html-popup.html","tooltip/templates/tooltip-popup.html","tooltip/templates/tooltip-template-popup.html","popover/templates/popover-html-popup.html","popover/templates/popover-popup.html","popover/templates/popover-template-popup.html","datepicker/templates/datepicker-calendar.html","datepicker/templates/datepicker.html","modal/templates/backdrop.html","modal/templates/window.html","notify/templates/notify.html","pager/templates/pager.html","searchBox/templates/searchBox.html","select/templates/choices.html","select/templates/match-multiple.html","select/templates/match.html","select/templates/select-multiple.html","select/templates/select.html","switch/templates/switch.html","timepicker/templates/timepicker-timepanel.html","timepicker/templates/timepicker.html","typeahead/templates/typeaheadTpl.html"]);
 /**
  * alert
  * 警告提示指令
@@ -6776,6 +6776,217 @@ angular.module('ui.xg.timepicker', ['ui.xg.timepanel', 'ui.xg.popover'])
         };
     });
 
+/**
+ * typeahead
+ * 搜索提示指令
+ * Author:heqingyang@meituan.com
+ * Date:2016-08-17
+ */
+
+angular.module('ui.xg.typeahead', [])
+    .filter('html', ['$sce', function ($sce) {
+        return function (input, type) {
+            if (angular.isString(input)) {
+                return $sce.trustAs(type || 'html', input);
+            } else {
+                return '';
+            }
+        };
+    }])
+    .controller('uixTypeaheadCtrl', ['$scope', '$attrs', '$element', '$document', '$q', '$log',
+        function ($scope, $attrs, $element, $document, $q, $log) {
+
+            var $ = angular.element;
+            var listElm = $('[uix-typeahead-popup]');
+            var ngModelCtrl = {$setViewValue: angular.noop};
+            var placeholder = angular.isDefined($scope.placeholder) ? $scope.placeholder : '';
+            var asyncFunc = $scope.$parent.$eval($attrs.getAsyncFunc);
+            var openScope = null;
+            var isResult = false;
+
+            $scope.$q = $q;
+            $scope.typeahead = {};
+            $scope.matchList = [];
+            $scope.queryList = $scope.queryList || [];
+            $scope.typeaheadLoading = $scope.typeaheadLoading || false;
+            $scope.typeaheadNoResults = $scope.typeaheadNoResults || false;
+            $scope.activeIndex = 0;
+            $scope.listLength = 0;
+            $scope.isShow = false;
+
+            this.elm = $element;
+            this.init = function (_ngModelCtrl) {
+                ngModelCtrl = _ngModelCtrl;
+                ngModelCtrl.$render = this.render;
+            };
+            this.render = function () {
+                $scope.typeahead.query = ngModelCtrl.$modelValue;
+            };
+
+            $element.addClass('.uix-typeahead');
+            $element.find('input').attr('placeholder', placeholder);
+            listElm.bind('click', function (evt) {
+                evt.preventDefault();
+                evt.stopPropagation();
+            });
+
+            // 监听typeahead.query变量,对matchList进行处理
+            $scope.$watch('typeahead.query', function (newValue) {
+                var Reg = new RegExp(newValue, 'gim');
+                ngModelCtrl.$setViewValue(newValue);
+                ngModelCtrl.$render();
+
+                if(isResult) {
+                    isResult = false;
+                    return;
+                }
+                if(newValue === '') {
+                    $scope.isShow = false;
+                    return;
+                }
+                $scope.matchList = [];
+
+                // 如果有异步处理函数,直接执行,否则使用本地数据
+                if(asyncFunc) {
+                    $scope.typeaheadLoading = true;
+                    $scope.$q.when(asyncFunc(newValue))
+                        .then(function (matches) {
+                            $scope.typeaheadLoading = false;
+                            $scope.matchList = matches.map(function (item) {
+                                return {'text': item, 'html': item};
+                            });
+                            if($scope.matchList.length === 0) {
+                                $scope.typeaheadNoResults = true;
+                            } else {
+                                $scope.typeaheadNoResults = false;
+                            }
+                            $scope.listLength = $scope.matchList.length;
+                            $scope.activeIndex = 0;
+                            $scope.isShow = ($scope.listLength > 0) ? true : false;
+                        }, function (error) {
+                            $scope.typeaheadLoading = false;
+                            $scope.typeaheadNoResults = true;
+                            $scope.activeIndex = 0;
+                            $scope.matchList = [];
+                            $scope.listLength = 0;
+                            $scope.isShow = false;
+                            $log.log(error);
+                        });
+                } else {
+                    $scope.queryList.forEach(function (item) {
+                        var match = item.match(Reg);
+                        if(match) {
+                            $scope.matchList.push({ 'text': item, 'html': parseNode(item, newValue)});
+                        }
+                    });
+                    if($scope.matchList.length === 0) {
+                        $scope.typeaheadNoResults = true;
+                    } else {
+                        $scope.typeaheadNoResults = false;
+                    }
+                    $scope.listLength = $scope.matchList.length;
+                    $scope.activeIndex = 0;
+                    $scope.isShow = ($scope.listLength > 0) ? true : false;
+                }
+            });
+
+            // 监听isShow变量,进行事件绑定控制
+            $scope.$watch('isShow', function (newValue) {
+                if (newValue) {
+                    if(!openScope) {
+                        $document.bind('click', closeTypeahead);
+                        $document.bind('keydown', arrowKeyBind);
+                    }
+                    openScope = $scope;
+                } else {
+                    $document.unbind('click', closeTypeahead);
+                    $document.unbind('keydown', arrowKeyBind);
+                    openScope = null;
+                }
+
+            });
+
+            $scope.selectItem = function (item) {
+                isResult = true;
+                $scope.typeahead.query = item.text;
+                $scope.isShow = false;
+            };
+
+            $scope.isActive = function (index) {
+                if(index === $scope.activeIndex) {
+                    return true;
+                } else {
+                    return false;
+                }
+            };
+
+            // document绑定点击事件
+            function closeTypeahead() {
+                if (!openScope) {
+                    return;
+                }
+                $scope.$apply(function () {
+                    $scope.isShow = false;
+                });
+            }
+
+            // document绑定键盘事件
+            function arrowKeyBind(evt) {
+                if(evt.which === 9 || evt.which === 13) {
+                    $scope.$apply(function () {
+                        $scope.selectItem($scope.matchList[$scope.activeIndex]);
+                    });
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                } else
+                if(evt.which === 40) {
+                    if($scope.activeIndex + 1 < $scope.listLength) {
+                        $scope.$apply(function () {
+                            $scope.activeIndex++;
+                        });
+                    }
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                } else
+                if(evt.which === 38) {
+                    if($scope.activeIndex > 0) {
+                        $scope.$apply(function () {
+                            $scope.activeIndex--;
+                        });
+                    }
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                }
+            }
+
+            // 代码高亮处理
+            function parseNode(item, str) {
+                var Reg = new RegExp(str, 'gim');
+                return item.replace(Reg, function (message) {return '<strong>' + message + '</strong>';});
+            }
+        }])
+    .directive('uixTypeahead', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                queryList: '=?',
+                placeholder: '@?',
+                getAsyncFunc: '&?',
+                typeaheadLoading: '=?',
+                typeaheadNoResults: '=?',
+                ngModel: '='
+            },
+            require: ['uixTypeahead', '?ngModel'],
+            controller: 'uixTypeaheadCtrl',
+            templateUrl: 'templates/typeaheadTpl.html',
+            link: function (scope, el, attrs, ctrls) {
+                var typeaheadCtrl = ctrls[0], ngModelCtrl = ctrls[1];
+                typeaheadCtrl.init(ngModelCtrl, el);
+            }
+        };
+    });
+
 angular.module("alert/templates/alert.html",[]).run(["$templateCache",function($templateCache){
     $templateCache.put("templates/alert.html",
     "<div ng-show=\"!defaultclose\" class=\"alert uix-alert\" ng-class=\"['alert-' + (type || 'warning'), closeable ? 'alert-dismissible' : null]\" role=\"alert\">"+
@@ -7209,6 +7420,18 @@ angular.module("timepicker/templates/timepicker.html",[]).run(["$templateCache",
     "            </button>"+
     "        </span>"+
     "    </div>"+
+    "</div>"+
+    "");
+}]);
+angular.module("typeahead/templates/typeaheadTpl.html",[]).run(["$templateCache",function($templateCache){
+    $templateCache.put("templates/typeaheadTpl.html",
+    "<div class=\"btn-group\">"+
+    "    <input type=\"text\" ng-model=\"typeahead.query\" class=\"input-sm form-control\">"+
+    "    <ul uix-typeahead-popup class=\"dropdown-menu typeahead-menu\" ng-show=\"isShow\">"+
+    "        <li ng-repeat=\"item in matchList track by $index\" ng-click=\"selectItem(item)\" ng-class=\"{active: isActive($index)}\">"+
+    "            <a ng-bind-html=\"item.html|html\"></a>"+
+    "        </li>"+
+    "    </ul>"+
     "</div>"+
     "");
 }]);
---
+++ src/typeahead/docs/readme.md
@@ -0,0 +1,37 @@
+# Typeahead
+## Description
+搜索提示组件.分为本地数据和异步数据两种.
+
+## Usage
+
+``` javascript
+基本用法
+<uix-typeahead ng-model="string"
+    [ query-list="list" ]
+    [ placeholder="string" ]
+    [ get-async-func="function" ]
+    [ typeahead-loading="boolean" ]
+    [ typeahead-no-results="boolean" ]>
+</uix-typeahead>
+```
+## Restrict
+- 'E'
+
+## Arguments
+
+- ngModel:typeahead的输入文字,改变后更新绑定的变量。
+    - type:`string`
+- queryList(optional):指定typeahead的筛选列表，传入一个字符串数组作为本地列表。
+    - type:`list`
+- placeholder(optional):指定typeahead中input的提示文字。
+    - type:`string`
+- getAsyncFunc(optional):如果需要显示异步数据时,getAsyncFunc指定数据提供function。
+    - type:`function`
+    - default:`false`
+- typeaheadLoading(optional):当getAsyncFunc传入时,该变量表示当前是否为loading状态。
+    - type:`boolean`
+    - default:`false`
+- typeaheadNoReuslts(optional):当getAsyncFunc传入时,该变量表示当前是否为空数据。
+	- type:`boolean`
+	- default:`"false"`
+
