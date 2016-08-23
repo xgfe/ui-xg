5f3dae53389fe09c72ccb5ef9616d21d638fb522
刘洋 <liuyang46@meituan.com>
2016-08-23 12:30:42 +0800

    feat(carousel):add new component. issue:#5

--- dist/js/ui-xg.js
+++ dist/js/ui-xg.js
@@ -1,6 +1,6 @@
 /*
  * ui-xg
- * Version: 1.1.0 - 2016-08-10
+ * Version: 1.1.0 - 2016-08-23
  * License: MIT
  */
 angular.module("ui.xg", ["ui.xg.tpls","ui.xg.alert","ui.xg.button","ui.xg.buttonGroup","ui.xg.timepanel","ui.xg.calendar","ui.xg.transition","ui.xg.carousel","ui.xg.collapse","ui.xg.position","ui.xg.datepicker","ui.xg.dropdown","ui.xg.loader","ui.xg.stackedMap","ui.xg.modal","ui.xg.notify","ui.xg.pager","ui.xg.tooltip","ui.xg.popover","ui.xg.searchBox","ui.xg.select","ui.xg.sortable","ui.xg.switch","ui.xg.tableLoader","ui.xg.timepicker"]);
@@ -1258,230 +1258,305 @@ angular.module('ui.xg.transition', [])
  * Date:2016-08-05
  */
 angular.module('ui.xg.carousel', ['ui.xg.transition'])
-	.controller('uixCarouselCtrl', ['$scope', '$timeout', '$uixTransition', function ($scope, $timeout, $uixTransition) {
+    .controller('uixCarouselCtrl', ['$scope', '$timeout', '$uixTransition', function ($scope, $timeout, $uixTransition) {
 
-		$scope.itemList = [];
-		$scope.indexNumber = 0;
-		$scope.currentItem = null;
-		$scope.pause = false;
+        $scope.itemList = [];
+        $scope.indexNumber = 0;
+        $scope.currentItem = null;
+        $scope.pause = false;
 
-		this.addItem = function (scope, ele, active) {
-			$scope.itemList.push({scope: scope, ele: ele});
+        var transition = null;
+        var transitionRemove = {
+            list: [],
+            done: function () {
+                var itemList = $scope.itemList;
 
-			if (!!active) {
-				$scope.index = scope.index;
-			}
+                // 双重循环,一个一个的删
+                for (var i = 0; i < this.list.length; i++) {
+                    for (var j = 0; j < itemList.length; j++) {
+                        if (this.list[i] === itemList[j]) {
+                            nextShow();
+                            itemList.splice(j, 1);
+                            break;
+                        }
+                    }
+                }
+            }
+        };
 
-			// 如果用户没传值,则默认第一个为初始显示
-			if ($scope.index === undefined) {
-				$scope.index = scope.index;
-			}
+        this.addItem = function (scope, ele, active) {
+            $scope.itemList.push({scope: scope, ele: ele});
 
-			// 设置默认显示
-			if (scope.index === $scope.index) {
+            if (active === true) {
+                $scope.index = scope.index;
+            }
 
-				// 防止多个item设置active
-				for (var i = 0; i < $scope.itemList.length - 1; i++) {
-					$scope.itemList[i].ele.removeClass('active');
-					$scope.itemList[i].scope.active = false;
-				}
+            // 如果用户没传值,则默认第一个为初始显示
+            if ($scope.index === angular.isUndefined) {
+                $scope.index = scope.index;
+            }
 
-				ele.addClass('active');
-				$scope.indexNumber = $scope.itemList.length - 1;
-				$scope.currentItem = $scope.itemList[$scope.indexNumber];
-			}
+            // 设置默认显示
+            if (scope.index === $scope.index) {
 
-		};
+                // 防止多个item设置active
+                for (var i = 0; i < $scope.itemList.length - 1; i++) {
+                    $scope.itemList[i].ele.removeClass('active');
+                    $scope.itemList[i].scope.active = false;
+                }
 
-		var transition = null;
-		// 点击更换次序
-		$scope.change = function (oldNum, newNum) {
+                ele.addClass('active');
+                $scope.indexNumber = $scope.itemList.length - 1;
+                $scope.currentItem = $scope.itemList[$scope.indexNumber];
+            }
 
-			if (transition !== null) {
+            // 触发一次
+            $scope.loop();
 
-				// 还在动画效果中,禁止点击
-				return true;
-			}
+        };
 
-			$scope.killLoop();
+        this.removeItem = function (scope, ele) {
 
-			$scope.indexNumber = newNum;
+            if (transition) {
+                transition.cancel();
+                transition = null;
+            }
 
-			var oldItem = $scope.itemList[oldNum];
-			var newItem = $scope.itemList[newNum];
+            var itemList = $scope.itemList;
+            for (var i = 0; i < itemList.length; i++) {
+                var item = itemList[i];
 
-			oldItem.scope.active = false;
-			newItem.scope.active = true;
+                if (item.scope === scope && item.ele === ele) {
 
-			if (!!$scope.noTransition) {
+                    // 需要判断当前item是否含有 active,prev,next。如果有证明当前或者即将显示自己,需要挪到下一帧
 
-				// 无动画效果
-				oldItem.ele.removeClass('active');
-				newItem.ele.addClass('active');
+                    if (scope.active === true) {
+                        $scope.indexNumber = i;
+                        nextShow();
+                        itemList.splice(i, 1);
+                    } else if (ele.hasClass('prev') || ele.hasClass('next')) {
 
-				$scope.loop();
-				return true;
-			}
+                        // 证明还在动画效果中,加入列表等待动画结束后删除
+                        transitionRemove.list.push(item);
+                    } else {
 
-			var nextCss = 'next';
-			var animateCss = 'left';
+                        // 直接删除
+                        itemList.splice(i, 1);
+                    }
 
-			if (oldNum > newNum) {
-				nextCss = 'prev';
-				animateCss = 'right';
-			}
-
-
-			// 加个timeout,让下一个item先就位,否则会出现无动画现象
-			newItem.ele.addClass(nextCss);
-
-			$timeout(function () {
-
-				// 监听oldItem
-				transition = $uixTransition(oldItem.ele, animateCss);
-				newItem.ele.addClass(animateCss);
-
-				transition.then(function () {
-					transitionDone(oldItem, newItem, nextCss, animateCss);
-				}, function () {
-					transitionDone(oldItem, newItem, nextCss, animateCss);
-				});
-
-				// 这里的50ms是用来防止chrome同时加入class丢失动画
-
-			}, 50);
+                    return true;
+                }
+            }
+        };
 
 
-		};
+        function nextShow() {
 
-		function transitionDone(oldItem, newItem, nextCss, animateCss) {
-			// 更换item显示
-			oldItem.ele.removeClass('active');
-			oldItem.ele.removeClass(animateCss);
-			newItem.ele.removeClass(nextCss);
-			newItem.ele.removeClass(animateCss);
-			newItem.ele.addClass('active');
-
-
-			transition = null;
-			$scope.loop();
-		}
-
-		var timer = null;
-		$scope.loop = function () {
-
-			if (!!$scope.noLoop || $scope.pause || $scope.interval === 0) {
-				return true;
-			}
-			// 先kill一次,防止多个定时器出现
-			$scope.killLoop();
-			timer = $timeout(function () {
-				$scope.next();
-			}, $scope.interval);
-		};
-
-		$scope.killLoop = function () {
-			if (timer !== null) {
-				$timeout.cancel(timer);
-				timer = null;
-			}
-
-			return true;
-		};
-
-		$scope.next = function () {
-
-			var oldNum = $scope.indexNumber;
-			var newNum = 1 + oldNum;
-
-			if (newNum >= $scope.itemList.length) {
-				newNum = 0;
-			}
-
-			$scope.change(oldNum, newNum);
-		};
-
-		$scope.prev = function () {
-
-			var oldNum = $scope.indexNumber;
-			var newNum = oldNum - 1;
-
-			if (newNum < 0) {
-				newNum = $scope.itemList.length - 1;
-			}
-
-			$scope.change(oldNum, newNum);
-		};
-
-	}])
-	.directive('uixCarousel', function () {
-		return {
-			restrict: 'AE',
-			templateUrl: 'templates/carousel.html',
-			replace: true,
-			scope: {
-				index: '=?active',
-				interval: '=?',
-				noLoop: '=?',
-				noPause: '=?',
-				noTransition: '=?'
-			},
-			controller: 'uixCarouselCtrl',
-			controllerAs: 'carouselCtrl',
-			transclude: true,
-			link: function (scope, ele) {
-				var interval = parseInt(scope.interval);
-				interval = isNaN(interval) ? 0 : interval;
-				scope.interval = interval;
+            // 当前显示状态,把active挪到下一帧
+            var noTransition = $scope.noTransition;
+            $scope.noTransition = true;
+            $scope.next();
+            $scope.noTransition = noTransition;
+        }
 
 
-				scope.$watch('noLoop', function (newValue,oldValue) {
+        // 点击更换次序
+        $scope.change = function (oldNum, newNum) {
 
-					if (!!newValue) {
-						scope.killLoop();
-					} else {
-						scope.noLoop = false;
-						scope.loop();
-					}
+            if (transition !== null) {
 
-				});
+                // 还在动画效果中,禁止点击
+                return true;
+            }
 
-				ele.on('mouseenter', function () {
-					if (scope.noPause) {
-						return true;
-					}
-					scope.pause = true;
-					scope.killLoop();
-				});
+            if (oldNum === newNum) {
+                $scope.itemList[oldNum].active = true;
+                $scope.itemList[oldNum].ele.addClass('active');
+                return true;
+            }
 
-				ele.on('mouseleave', function () {
-					if (scope.noPause) {
-						return true;
-					}
-					scope.pause = false;
-					scope.loop();
-				});
+            $scope.killLoop();
+
+            $scope.indexNumber = newNum;
+
+            var oldItem = $scope.itemList[oldNum];
+            var newItem = $scope.itemList[newNum];
+
+            oldItem.scope.active = false;
+            newItem.scope.active = true;
+
+            if ($scope.noTransition === true) {
+
+                // 无动画效果
+                oldItem.ele.removeClass('active');
+                newItem.ele.addClass('active');
+
+                $scope.loop();
+                return true;
+            }
+
+            var nextCss = 'next';
+            var animateCss = 'left';
+
+            if (oldNum > newNum) {
+                nextCss = 'prev';
+                animateCss = 'right';
+            }
 
 
-			}
-		}
-	})
-	.directive('uixCarouselItem', function () {
-		return {
-			restrict: 'AE',
-			templateUrl: 'templates/carousel-item.html',
-			require: '^uixCarousel',
-			scope: {
-				index: '=',
-				active: '=?'
-			},
-			transclude: true,
-			link: function (scope, el, attrs, carouselCtrl) {
-				carouselCtrl.addItem(scope, el, scope.active);
-				el.addClass('item');
-			}
-		}
-	});
+            // 加个timeout,让下一个item先就位,否则会出现无动画现象
+            newItem.ele.addClass(nextCss);
+
+            $timeout(function () {
+
+                // 监听oldItem
+                transition = $uixTransition(oldItem.ele, animateCss);
+                newItem.ele.addClass(animateCss);
+
+                transition.then(function () {
+                    transitionDone(oldItem, newItem, nextCss, animateCss);
+                }, function () {
+                    transitionDone(oldItem, newItem, nextCss, animateCss);
+                });
+
+                // 这里的50ms是用来防止chrome同时加入class丢失动画
+
+            }, 50);
+
+        };
+
+        function transitionDone(oldItem, newItem, nextCss, animateCss) {
+            // 更换item显示
+            oldItem.ele.removeClass('active');
+            oldItem.ele.removeClass(animateCss);
+            newItem.ele.removeClass(nextCss);
+            newItem.ele.removeClass(animateCss);
+            newItem.ele.addClass('active');
+
+
+            transition = null;
+            $scope.loop();
+            transitionRemove.done();
+        }
+
+        var timer = null;
+        $scope.loop = function () {
+
+            if (!!$scope.noLoop || $scope.pause || $scope.interval === 0) {
+                return true;
+            }
+            // 先kill一次,防止多个定时器出现
+            $scope.killLoop();
+            timer = $timeout(function () {
+                $scope.next();
+            }, $scope.interval);
+        };
+
+        $scope.killLoop = function () {
+            if (timer !== null) {
+                $timeout.cancel(timer);
+                timer = null;
+            }
+
+            return true;
+        };
+
+        $scope.next = function () {
+            var oldNum = $scope.indexNumber;
+            var newNum = 1 + oldNum;
+
+            if (newNum >= $scope.itemList.length) {
+                newNum = 0;
+            }
+
+            $scope.change(oldNum, newNum);
+        };
+
+        $scope.prev = function () {
+
+            var oldNum = $scope.indexNumber;
+            var newNum = oldNum - 1;
+
+            if (newNum < 0) {
+                newNum = $scope.itemList.length - 1;
+            }
+
+            $scope.change(oldNum, newNum);
+        };
+
+    }])
+    .directive('uixCarousel', function () {
+        return {
+            restrict: 'AE',
+            templateUrl: 'templates/carousel.html',
+            replace: true,
+            scope: {
+                index: '=?active',
+                interval: '=?',
+                noLoop: '=?',
+                noPause: '=?',
+                noTransition: '=?'
+            },
+            controller: 'uixCarouselCtrl',
+            controllerAs: 'carouselCtrl',
+            transclude: true,
+            link: function (scope, ele) {
+                var interval = parseInt(scope.interval, 10);
+                interval = isNaN(interval) ? 0 : interval;
+                scope.interval = interval;
+
+
+                scope.$watch('noLoop', function (newValue) {
+
+                    if (newValue === true) {
+                        scope.killLoop();
+                    } else {
+                        scope.noLoop = false;
+                        scope.loop();
+                    }
+
+                });
+
+                ele.on('mouseenter', function () {
+                    if (scope.noPause) {
+                        return true;
+                    }
+                    scope.pause = true;
+                    scope.killLoop();
+                });
+
+                ele.on('mouseleave', function () {
+                    if (scope.noPause) {
+                        return true;
+                    }
+                    scope.pause = false;
+                    scope.loop();
+                });
+
+
+            }
+        };
+    })
+    .directive('uixCarouselItem', function () {
+        return {
+            restrict: 'AE',
+            templateUrl: 'templates/carousel-item.html',
+            require: '^uixCarousel',
+            scope: {
+                index: '=',
+                active: '=?'
+            },
+            transclude: true,
+            link: function (scope, el, attrs, carouselCtrl) {
+                carouselCtrl.addItem(scope, el, scope.active);
+                el.addClass('item');
+
+                scope.$on('$destroy', function () {
+                    carouselCtrl.removeItem(scope, el);
+                });
+            }
+        };
+    });
 
 /**
  * collapse
--- src/carousel/docs/script.js
+++ src/carousel/docs/script.js
@@ -1,42 +1,69 @@
 angular.module('uixDemo').controller('carouselDemoCtrl', ['$scope', function ($scope) {
-	$scope.active = 2;
-	$scope.imgs = [
-		{
-			src: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzkwMHg1MDAvYXV0by8jNzc3OiM1NTUvdGV4dDpGaXJzdCBzbGlkZQpDcmVhdGVkIHdpdGggSG9sZGVyLmpzIDIuNi4wLgpMZWFybiBtb3JlIGF0IGh0dHA6Ly9ob2xkZXJqcy5jb20KKGMpIDIwMTItMjAxNSBJdmFuIE1hbG9waW5za3kgLSBodHRwOi8vaW1za3kuY28KLS0+PGRlZnM+PHN0eWxlIHR5cGU9InRleHQvY3NzIj48IVtDREFUQVsjaG9sZGVyXzE1NjU5OTBmYWZmIHRleHQgeyBmaWxsOiM1NTU7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6NDVwdCB9IF1dPjwvc3R5bGU+PC9kZWZzPjxnIGlkPSJob2xkZXJfMTU2NTk5MGZhZmYiPjxyZWN0IHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIiBmaWxsPSIjNzc3Ii8+PGc+PHRleHQgeD0iMzA4LjI5Njg3NSIgeT0iMjcwLjEiPkZpcnN0IHNsaWRlPC90ZXh0PjwvZz48L2c+PC9zdmc+',
-			index: 0,
-			active: false
-		},
-		{
-			src: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzkwMHg1MDAvYXV0by8jNjY2OiM0NDQvdGV4dDpTZWNvbmQgc2xpZGUKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTY1OTkxMTU0MCB0ZXh0IHsgZmlsbDojNDQ0O2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjQ1cHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NjU5OTExNTQwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iIzY2NiIvPjxnPjx0ZXh0IHg9IjI2NC45NTMxMjUiIHk9IjI3MC4xIj5TZWNvbmQgc2xpZGU8L3RleHQ+PC9nPjwvZz48L3N2Zz4=',
-			index: 1,
-			active: true
-		},
-		{
-			src: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAwIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzkwMHg1MDAvYXV0by8jNTU1OiMzMzMvdGV4dDpUaGlyZCBzbGlkZQpDcmVhdGVkIHdpdGggSG9sZGVyLmpzIDIuNi4wLgpMZWFybiBtb3JlIGF0IGh0dHA6Ly9ob2xkZXJqcy5jb20KKGMpIDIwMTItMjAxNSBJdmFuIE1hbG9waW5za3kgLSBodHRwOi8vaW1za3kuY28KLS0+PGRlZnM+PHN0eWxlIHR5cGU9InRleHQvY3NzIj48IVtDREFUQVsjaG9sZGVyXzE1NjU5OTBlZWE4IHRleHQgeyBmaWxsOiMzMzM7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6NDVwdCB9IF1dPjwvc3R5bGU+PC9kZWZzPjxnIGlkPSJob2xkZXJfMTU2NTk5MGVlYTgiPjxyZWN0IHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwIiBmaWxsPSIjNTU1Ii8+PGc+PHRleHQgeD0iMjk4LjMyMDMxMjUiIHk9IjI3MC4xIj5UaGlyZCBzbGlkZTwvdGV4dD48L2c+PC9nPjwvc3ZnPg==',
-			index: 2,
-			active: false
-		}
-	];
+    $scope.active = 2;
+    $scope.imgs = [
+        {
+            src: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Inllc' +
+            'yI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0' +
+            'iMCAwIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzkwMHg1MDAvYX' +
+            'V0by8jNzc3OiM1NTUvdGV4dDpGaXJzdCBzbGlkZQpDcmVhdGVkIHdpdGggSG9sZGVyLmpzIDIuNi4wLgpMZWFybiBtb3JlIGF0IGh0dHA' +
+            '6Ly9ob2xkZXJqcy5jb20KKGMpIDIwMTItMjAxNSBJdmFuIE1hbG9waW5za3kgLSBodHRwOi8vaW1za3kuY28KLS0+PGRlZnM+PHN0eWxl' +
+            'IHR5cGU9InRleHQvY3NzIj48IVtDREFUQVsjaG9sZGVyXzE1NjU5OTBmYWZmIHRleHQgeyBmaWxsOiM1NTU7Zm9udC13ZWlnaHQ6Ym9' +
+            'sZDtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6ND' +
+            'VwdCB9IF1dPjwvc3R5bGU+PC9kZWZzPjxnIGlkPSJob2xkZXJfMTU2NTk5MGZhZmYiPjxyZWN0IHdpZHRoPSI5MDAiIGhlaWdodD0i' +
+            'TAwIiBmaWxsPSIjNzc3Ii8+PGc+PHRleHQgeD0iMzA4LjI5Njg3NSIgeT0iMjcwLjEiPkZpcnN0IHNsaWRlPC90ZXh0PjwvZz48L2c+' +
+            'PC9zdmc+',
+            index: 0,
+            active: false
+        },
+        {
+            src: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/' +
+            'PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCAw' +
+            'IDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzkwMHg1MDAvYXV0by8j' +
+            'NjY2OiM0NDQvdGV4dDpTZWNvbmQgc2xpZGUKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8v' +
+            'aG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0' +
+            'eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTY1OTkxMTU0MCB0ZXh0IHsgZmlsbDojNDQ0O2ZvbnQtd2VpZ2h0OmJvbGQ7' +
+            'Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjQ1cHQ' +
+            'gfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1NjU5OTExNTQwIj48cmVjdCB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMC' +
+            'IgZmlsbD0iIzY2NiIvPjxnPjx0ZXh0IHg9IjI2NC45NTMxMjUiIHk9IjI3MC4xIj5TZWNvbmQgc2xpZGU8L3RleHQ+PC9nPjwvZz48L' +
+            '3N2Zz4=',
+            index: 1,
+            active: true
+        },
+        {
+            src: 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/' +
+            'PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iOTAwIiBoZWlnaHQ9IjUwMCIgdmlld0JveD0iMCA' +
+            'wIDkwMCA1MDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzkwMHg1MDAvYXV0by8' +
+            'jNTU1OiMzMzMvdGV4dDpUaGlyZCBzbGlkZQpDcmVhdGVkIHdpdGggSG9sZGVyLmpzIDIuNi4wLgpMZWFybiBtb3JlIGF0IGh0dHA6Ly' +
+            '9ob2xkZXJqcy5jb20KKGMpIDIwMTItMjAxNSBJdmFuIE1hbG9waW5za3kgLSBodHRwOi8vaW1za3kuY28KLS0+PGRlZnM+PHN0eWxlI' +
+            'HR5cGU9InRleHQvY3NzIj48IVtDREFUQVsjaG9sZGVyXzE1NjU5OTBlZWE4IHRleHQgeyBmaWxsOiMzMzM7Zm9udC13ZWlnaHQ6Ym9sZ' +
+            'Dtmb250LWZhbWlseTpBcmlhbCwgSGVsdmV0aWNhLCBPcGVuIFNhbnMsIHNhbnMtc2VyaWYsIG1vbm9zcGFjZTtmb250LXNpemU6NDVwd' +
+            'CB9IF1dPjwvc3R5bGU+PC9kZWZzPjxnIGlkPSJob2xkZXJfMTU2NTk5MGVlYTgiPjxyZWN0IHdpZHRoPSI5MDAiIGhlaWdodD0iNTAwI' +
+            'iBmaWxsPSIjNTU1Ii8+PGc+PHRleHQgeD0iMjk4LjMyMDMxMjUiIHk9IjI3MC4xIj5UaGlyZCBzbGlkZTwvdGV4dD48L2c+PC9nPjwv' +
+            'c3ZnPg==',
+            index: 2,
+            active: false
+        }
+    ];
 
 
-	$scope.interval = 1000;
+    $scope.interval = 1000;
 
 
-	$scope.noLoop = false;
+    $scope.noLoop = false;
 
-	$scope.changeLoop = function () {
-		$scope.noLoop = !$scope.noLoop;
-	}
+    $scope.changeLoop = function () {
+        $scope.noLoop = !$scope.noLoop;
+    };
 
-	$scope.noPause = false;
+    $scope.noPause = false;
 
-	$scope.changePause = function () {
-		$scope.noPause = !$scope.noPause;
-	}
+    $scope.changePause = function () {
+        $scope.noPause = !$scope.noPause;
+    };
 
-	$scope.noTransition = false;
+    $scope.noTransition = false;
 
-	$scope.changeTransition = function () {
-		$scope.noTransition = !$scope.noTransition;
-	}
+    $scope.changeTransition = function () {
+        $scope.noTransition = !$scope.noTransition;
+    };
 }]);
