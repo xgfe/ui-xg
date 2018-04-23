1801e5418d5b92b905f008abc3b97f67b1d03cd2
杨继园 <YJY972080142@gmail.com>
2018-04-23 18:14:41 +0800

    chore(uixLoader): fix loader;fix #32

--- dist/js/ui-xg.js
+++ dist/js/ui-xg.js
@@ -1,6 +1,6 @@
 /*
  * ui-xg
- * Version: 2.1.7 - 2018-04-19
+ * Version: 2.1.8 - 2018-04-23
  * License: MIT
  */
 angular.module("ui.xg", ["ui.xg.tpls","ui.xg.transition","ui.xg.collapse","ui.xg.accordion","ui.xg.alert","ui.xg.button","ui.xg.buttonGroup","ui.xg.timepanel","ui.xg.calendar","ui.xg.carousel","ui.xg.position","ui.xg.stackedMap","ui.xg.tooltip","ui.xg.popover","ui.xg.dropdown","ui.xg.cityselect","ui.xg.datepicker","ui.xg.loader","ui.xg.modal","ui.xg.notify","ui.xg.pager","ui.xg.progressbar","ui.xg.rate","ui.xg.searchBox","ui.xg.select","ui.xg.sortable","ui.xg.step","ui.xg.steps","ui.xg.switch","ui.xg.tableLoader","ui.xg.tabs","ui.xg.timepicker","ui.xg.typeahead"]);
