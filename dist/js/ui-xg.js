53031061dd7cbae30a48cdb5b3c79bb6cfc23e51
杨继园 <YJY972080142@gmail.com>
2019-08-01 20:15:57 +0800

    fix(tooltip): add close delay

--- dist/js/ui-xg.js
+++ dist/js/ui-xg.js
@@ -1,6 +1,6 @@
 /*
  * ui-xg
- * Version: 2.1.11 - 2019-08-01
+ * Version: 2.1.12 - 2019-08-01
  * License: MIT
  */
 angular.module("ui.xg", ["ui.xg.tpls","ui.xg.transition","ui.xg.collapse","ui.xg.accordion","ui.xg.alert","ui.xg.avatar","ui.xg.button","ui.xg.buttonGroup","ui.xg.timepanel","ui.xg.calendar","ui.xg.carousel","ui.xg.position","ui.xg.stackedMap","ui.xg.tooltip","ui.xg.popover","ui.xg.dropdown","ui.xg.cityselect","ui.xg.datepicker","ui.xg.grid","ui.xg.loader","ui.xg.modal","ui.xg.notify","ui.xg.pager","ui.xg.progressbar","ui.xg.rate","ui.xg.searchBox","ui.xg.select","ui.xg.sortable","ui.xg.step","ui.xg.steps","ui.xg.switch","ui.xg.tableLoader","ui.xg.tabs","ui.xg.timeline","ui.xg.timepicker","ui.xg.typeahead"]);
@@ -2484,7 +2484,7 @@ angular.module('ui.xg.tooltip', ['ui.xg.position', 'ui.xg.stackedMap'])
     placementClassPrefix: '',
     animation: true,
     popupDelay: 0,
-    popupCloseDelay: 0,
+    popupCloseDelay: 200,
     useContentExp: false
   }; // Default hide triggers for each show trigger
 
