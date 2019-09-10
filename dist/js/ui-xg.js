1dcf3a4f96944f5e3876d84da17ac03b88d71804
杨继园 <YJY972080142@gmail.com>
2019-09-10 17:36:57 +0800

    chore(build): rebuild v2.1.14

--- dist/js/ui-xg.js
+++ dist/js/ui-xg.js
@@ -1,6 +1,6 @@
 /*
  * ui-xg
- * Version: 2.1.13 - 2019-09-10
+ * Version: 2.1.14 - 2019-09-10
  * License: MIT
  */
 angular.module("ui.xg", ["ui.xg.tpls","ui.xg.transition","ui.xg.collapse","ui.xg.accordion","ui.xg.alert","ui.xg.avatar","ui.xg.button","ui.xg.buttonGroup","ui.xg.timepanel","ui.xg.calendar","ui.xg.carousel","ui.xg.position","ui.xg.stackedMap","ui.xg.tooltip","ui.xg.popover","ui.xg.dropdown","ui.xg.cityselect","ui.xg.datatable","ui.xg.datepicker","ui.xg.grid","ui.xg.loader","ui.xg.modal","ui.xg.notify","ui.xg.pager","ui.xg.progressbar","ui.xg.rate","ui.xg.searchBox","ui.xg.select","ui.xg.sortable","ui.xg.step","ui.xg.steps","ui.xg.switch","ui.xg.tableLoader","ui.xg.tabs","ui.xg.timeline","ui.xg.timepicker","ui.xg.typeahead"]);
